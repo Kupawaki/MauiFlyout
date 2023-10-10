837c1c36d216c380748265bfc41c12b4d5f789fe
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-10-10 14:49:57 -0500

    Add project files.

---
+++ MauiFlyout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFlyout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiFlyout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiFlyout
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
